0685b6bd31aba96019ba438136c8abaf69a3b4ee
DMG-01 <110745735+DMG-01@users.noreply.github.com>
2024-08-06 12:59:41 +0100

    inventory html css and js

--- public/js/usersProfile/usersProfile.js
+++ public/js/usersProfile/usersProfile.js
@@ -11,7 +11,11 @@ personalInformationEl.onclick = personalInformation
 let orderEl = document.getElementById("orders")
 orderEl.onclick = orders
 
+let inventoryEl = document.getElementById("inventory")
+inventoryEl.onclick = inventory
+
 function personalInformation() {
+        document.getElementsByClassName("inventory")[0].style.display = "none"
     if(window.innerWidth <= 768) {
 document.getElementsByClassName("dashNav")[0].style.display = "none"
 document.getElementsByClassName("Orders")[0].style.display = "none"
@@ -23,10 +27,12 @@ document.getElementsByClassName("Orders")[0].style.display = "none"
     personalInformationEl.style.borderTopLeftRadius = "0px"
     document.getElementsByClassName("Orders")[0].style.display = "none"
     orderEl.style.borderLeft = "0px"
+    inventoryEl.style.borderLeft = "0px"
 }
 }
 
 function orders()  {
+     document.getElementsByClassName("inventory")[0].style.display = "none"
     if(window.innerWidth <= 768) {
 document.getElementsByClassName("dashNav")[0].style.display = "none"
 document.getElementsByClassName("personalPersonalInformation")[0].style.display = "none"
@@ -35,10 +41,30 @@ document.getElementsByClassName("Orders")[0].style.display = "block"
     orderEl.style.borderLeft = "10px solid #49C01F"
     orderEl.style.borderBottomLeftRadius = "0px"
     orderEl.style.borderTopLeftRadius = "0px"
+    inventoryEl.style.borderLeft = "0px"
         document.getElementsByClassName("Orders")[0].style.display = "block"
     personalInformationEl.style.borderLeft = "0px"
     document.getElementsByClassName("personalPersonalInformation")[0].style.display = "none"
+       
 }
+}
+
+function inventory() {
+   
+document.getElementsByClassName("personalPersonalInformation")[0].style.display = "none"
+document.getElementsByClassName("Orders")[0].style.display = "none"
+    document.getElementsByClassName("inventory")[0].style.display = "block"
+
+if(window.innerWidth <= 768) {
+document.getElementsByClassName("dashNav")[0].style.display = "none"
+}else {
+    inventoryEl.style.borderLeft = "10px solid #49C01F"
+        orderEl.style.borderLeft = "0px"
+    inventoryEl.style.borderBottomLeftRadius = "0px"
+    inventoryEl.style.borderTopLeftRadius = "0px"
+        personalInformationEl.style.borderLeft = "0px"
+}
+
 }
 
 
