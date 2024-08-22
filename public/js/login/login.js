ab24bb1c420f4ee2c1f0e387dea29264a21bd1fd
DMG-01 <110745735+DMG-01@users.noreply.github.com>
2024-08-22 13:18:21 +0100

    style for  forget password

---
+++ public/js/login/login.js
@@ -0,0 +1,4 @@
+document.getElementById("forgetPasswordButton").onclick = () => {
+    document.getElementsByClassNames("forgetPassword")[0].style.display = "flex";
+    document.getElementsByClassNames("forgetPassword")[0].style.position  
+}
