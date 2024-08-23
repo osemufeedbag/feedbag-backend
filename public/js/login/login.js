4a5c694f8ca60c08c62c1072e672fe7a2dd7b23d
DMG-01 <110745735+DMG-01@users.noreply.github.com>
2024-08-23 15:59:50 +0100

    forget password page

--- public/js/login/login.js
+++ public/js/login/login.js
@@ -1,4 +1,19 @@
-document.getElementById("forgetPasswordButton").onclick = () => {
-    document.getElementsByClassNames("forgetPassword")[0].style.display = "flex";
-    document.getElementsByClassNames("forgetPassword")[0].style.position  
+document.getElementById("forgetPasswordButton").onclick = (event) => {
+
+
+    document.getElementsByClassName("forgetPassword")[0].style.display = "block";
+    document.getElementsByClassName("forgetPassword")[0].style.position = "absolute";
+    
+    document.getElementsByClassName("left")[0].style.filter = "blur(8px)";
+    document.getElementsByClassName("left")[0].style.opacity = "0.8";
+
+    document.getElementsByClassName("heading")[0].style.filter = "blur(8px)";
+    document.getElementsByClassName("heading")[0].style.opacity = "0.8";
+
+    document.getElementsByClassName("forms")[0].style.filter = "blur(8px)";
+    document.getElementsByClassName("forms")[0].style.opacity = "0.8";
+};
+
+document.getElementById("cancel").onclick = ()=> {
+    location.reload();
 }
